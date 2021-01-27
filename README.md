e05266b0849dde11ab4cb3aa92bc0a166fa62125
Marco Ferrati <marco.ferrati@gmail.com>
2021-01-27 15:17:28 +0100

    Improve script

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # vrp-csp
 Solution to the Constraint Satisfaction Problem "Vehicle Routing Problem".
+
+## What main.py does
+Executes the model written in `model.mzn` with gecode solver for all the instances available in `instances/`. Every instance is executed for 10s, 30s, 1m, 2m, 5m and 10m.
+
+## Install MiniZinc-python
+1. [Optional] Create a virtual environment with virtualenv and activate it;
+2. Install the requirements with `pip install -r requirements.txt`
+
+## Execute the Python script
+```bash
+python main.py [-d dataset name] [-t time-limit in seconds] [-s index of the heuristic]
+```
+
+### Optional arguments
+1. Dataset name choose between:
+    - example
+    - mini-example
+    - pr01
+    - pr02
+    - pr03
+    - pr04
+    - pr05
+    - pr06
+    - pr07
+    - pr08
+    - pr09
+    - pr10
+    - pr11
+2. Time limit in seconds
+3. Index of the search strategy:
+    - 0 - Default search
+
--- main.py
+++ main.py
@@ -4,40 +4,61 @@
 
 import minizinc
 from datetime import timedelta
+from sys import argv
 
 # Time limits for running the problem and getting more interesting statistics.
-time_limits = [10, 30, 60, 120, 300, 600]
+if '-t' in argv:
+    time_limits = [int(argv[argv.index('-t') + 1])]
+else:
+    time_limits = [10, 30, 60, 120, 300, 600]
 
 # Test datasets for the model
-datasets = ["example", "mini-example", "pr01", "pr02", "pr03", "pr04", "pr05", "pr06", "pr07", "pr08", "pr09", "pr10", "pr11"]
+if '-d' in argv:
+    datasets = [argv[argv.index('-d') + 1]]
+else:
+    datasets = ["example", "mini-example", "pr01", "pr02", "pr03", "pr04", "pr05", "pr06", "pr07", "pr08", "pr09", "pr10", "pr11"]
+
+# Search strategies
+search_strategies = [
+    "",
+    "::int_search(next, dom_w_deg, indomain_random) :: restart_luby(10)"
+]
+if '-s' in argv:
+    search_strategies = [search_strategies[int(argv[argv.index('-s') + 1])]]
+
 
 print("----------\nVRP Script\n----------\n")
 
 # MiniZinc parameters
-model = minizinc.Model("./model.mzn")
 gecode = minizinc.Solver.lookup("gecode")
 
-for time_limit in time_limits:
+for search_strategy in search_strategies:
+    model = minizinc.Model("./model.mzn")
+    model.add_string("solve {} minimize obj_f;".format(search_strategy))
     print("-------------------------------------------")
-    print("Time limit for executing set to {} seconds".format(time_limit))
+    print("Search strategy: ", search_strategy)
     print("-------------------------------------------\n")
-    for dataset in datasets:
-        instance = minizinc.Instance(gecode, model)
-        instance.add_file("./instances/{}.dzn".format(dataset))
+    for time_limit in time_limits:
+        print("-------------------------------------------")
+        print("Time limit for executing set to {} seconds".format(time_limit))
+        print("-------------------------------------------\n")
+        for dataset in datasets:
+            instance = minizinc.Instance(gecode, model)
+            instance.add_file("./instances/{}.dzn".format(dataset))
 
-        result = instance.solve(timeout=timedelta(seconds=time_limit))
-        print("Instance: ", dataset)
-        print("---------------------------")
-        keys = result.statistics.keys()
-        print("Solve time: ", result.statistics['time'].total_seconds() if 'time' in keys else "Not available.")
-        print("Failures: ", result.statistics['failures'] if 'failures' in keys else "Not available.")
-        print("Restarts: ", result.statistics['restarts'] if 'restarts' in keys else "Not available.")
-        print("---------------------------")
-        if result.status.has_solution():
-            variables = str(result.solution).split("\n")
-            print("next: ", variables[0])
-            print("vehicle: ", variables[1])
-            print("used_vehicles: ", variables[2])
-            print("total_distance: ", variables[3], "\n")
-        else:
-            print("No solutions were found (", result.status, ")\n")
+            result = instance.solve(timeout=timedelta(seconds=time_limit))
+            print("Instance: ", dataset)
+            print("---------------------------")
+            keys = result.statistics.keys()
+            print("Solve time: ", result.statistics['time'].total_seconds() if 'time' in keys else "Not available.")
+            print("Failures: ", result.statistics['failures'] if 'failures' in keys else "Not available.")
+            print("Restarts: ", result.statistics['restarts'] if 'restarts' in keys else "Not available.")
+            print("---------------------------")
+            if result.status.has_solution():
+                variables = str(result.solution).split("\n")
+                print("next: ", variables[0])
+                print("vehicle: ", variables[1])
+                print("used_vehicles: ", variables[2])
+                print("total_distance: ", variables[3], "\n")
+            else:
+                print("No solutions were found (", result.status, ")\n")
